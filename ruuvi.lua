642f0d59ac55bfe7f2ef2785a2c1ebaf269ef241
Stæld Lakorv <staeld@staeld.co.cc>
2013-01-11 20:58:16 +0100

    Initial commit

---
+++ ruuvi.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env lua
+-- ruuvi.lua - Bindings for accessing data from the RuuviTracker via the client API
+-- Copyright Stæld Lakorv, 2013 <staeld@illumine.ch>
+--
+-- This file is part of lua-ruuvi.
+--
+-- lua-ruuvi is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- lua-ruuvi is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with lua-ruuvi. If not, see <http://www.gnu.org/licenses/>.
+
+
+-- EOF
